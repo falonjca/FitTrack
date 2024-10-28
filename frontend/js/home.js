b66a93d1a202cc87551b325237b36c35b9bcec1c
Luis Diego Nuñez <nunezbrenesl@gmail.com>
2024-10-28 16:41:58 -0600

    Subo modificaciones del codigo de login y agrego funcionalidad a crear rutinas

---
+++ frontend/js/home.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+    
+    localStorage.getItem('UserId'); 
+
+    console.log('UserId obtenido ', userId);
+
+    if (!userId) {
+        Swal.fire({
+            title: 'Error',
+            text: 'No se encontró el ID del usuario',
+            icon: 'error'
+        }).then(() => {
+
+            window.location.href = '/login.html';
+
+        });
+        return;
+    }
+
+});
--- frontend/js/inicioSesion.js
+++ frontend/js/inicioSesion.js
@@ -3,34 +3,60 @@ const login = document.getElementById('iniciarSesionForm');
 login.addEventListener('submit', async (e) => {
     e.preventDefault();
 
+    const email = document.getElementById('correo').value;
+
     const datos = {
-        Email: document.getElementById('correo').value,
-    }
+        Email: email
+    };
 
-    try {
-    const respuesta = await fetch('http://localhost:3000/api/usuarios/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(datos)
-    });
-
-        const result = await respuesta.json();
-        Swal.fire({
-            icon: 'success',
-            title: 'Inicio de sesión exitoso',
-            confirmButtonText: "Aceptar",
-        });
-
-        console.log(result);
-
-    } catch (error) {
-        error = await respuesta.json();
+    if (
+        !email.match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        )
+    ) {
         Swal.fire({
             icon: 'error',
             title: 'Error',
-            text: 'error', 
+            text: 'El correo no es válido',
+            confirmButtonText: "Aceptar",
+        });
+        return; 
+    }
+
+    try {
+        const respuesta = await fetch('http://localhost:3000/api/usuarios/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(datos)
+        });
+
+        const result = await respuesta.json();
+
+        if (respuesta.ok) {
+            localStorage.setItem('UserId', result.User.UserId);
+        
+            Swal.fire({
+                icon: 'success',
+                title: 'Inicio de sesión exitoso',
+                confirmButtonText: "Aceptar",
+            }).then(() => {
+                window.location.href = 'home.html'; 
+            });
+        } else {
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: result.error,
+                confirmButtonText: "Aceptar",
+            });
+        }
+    } catch (error) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: 'Ocurrió un error en la conexión',
             confirmButtonText: "Aceptar",
         });
     }
